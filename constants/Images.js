b5f70665ec4a4b14c39d0ad5eee69d398fbea26a
Angelo Ortiz Vega <angelo2898@gmail.com>
2020-11-22 16:32:34 -0600

    [ADD] Project Structure

---
+++ constants/Images.js
@@ -0,0 +1,8 @@
+// local imgs
+const Logo = require("../assets/imgs/yanomas-logo.png");
+const LogoOnBoarding = require("../assets/imgs/yanomas-logo-onboarding.png");
+
+export default{
+    Logo,
+    LogoOnBoarding
+}
---
+++ constants/Theme.js
@@ -0,0 +1,10 @@
+export default {
+    COLORS: {
+        DEFAULT: '#D7B0C2',
+        PRIMARY: '#3E7A93',
+        SECONDARY: '#8BA7C4',
+        BORDER: '#CAD1D7',
+        WHITE: '#FFFFFF',
+        BLACK: '#000000'
+    }
+}
