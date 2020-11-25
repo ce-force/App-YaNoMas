eb4bf8b0de31ccd1e8b946e1ae1fda4ea27fc32e
Angelo Ortiz Vega <angelo2898@gmail.com>
2020-11-24 18:18:22 -0600

    [UPDATE] Settings tab initial interface is added

--- src/constants/Images.js
+++ src/constants/Images.js
@@ -1,8 +1,14 @@
 // local imgs
-const Logo = require("../assets/imgs/yanomas-logo.png");
-const LogoOnBoarding = require("../assets/imgs/yanomas-logo-onboarding.png");
+const Logo = require("../../assets/imgs/yanomas-logo.png");
+const LogoOnBoarding = require("../../assets/imgs/yanomas-logo-onboarding.png");
+
+const ProfileBackground = require("../../assets/imgs/profile-screen-bg.png");
+const ProfilePicture = 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80';
+
 
 export default{
     Logo,
-    LogoOnBoarding
+    LogoOnBoarding,
+    ProfileBackground,
+    ProfilePicture
 }
