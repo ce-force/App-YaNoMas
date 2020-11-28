71a36cb7e3da1ad0ccdcd625695f598e9f5500b3
Jess <jessicaes2000@gmail.com>
2020-11-27 20:50:07 -0600

    [UPDATE] Refresh added

--- src/screens/CircleScreen2.js
+++ src/screens/CircleScreen2.js
@@ -208,7 +208,10 @@ export default function CircleScreen() {
           </View>)
           :
           (
-              <ScrollView>
+              <ScrollView
+                  refreshControl={
+                    <RefreshControl refreshing={refreshing} onRefresh={fetchData} />
+                  }>
                 <Title>Mi círculo ({memberCount}/6)</Title>
                 {myCircle}
               </ScrollView>
